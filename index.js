720013be5656f0cae2d8534a61388fc201a1c63d
CrisDa7 <118973836+CrisDaa7@users.noreply.github.com>
2023-01-11 21:06:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Btn = document.querySelector('#Btn');
 
 Btn.addEventListener('click',function () {
-    alert('Si tu quieres este abrazo virtual lo podemos hacer en fisico ')
+    alert('Si tu quieres este abrazo virtual lo podemos hacer en fisico,si no funciona el programa disculparasme, esque aun estoy aprendiendo :C jaja')
 });
